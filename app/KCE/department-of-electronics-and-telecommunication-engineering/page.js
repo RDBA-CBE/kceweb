f9cd6ae639cd6ab264d9629cd01fae071e8b4c16
Pragathi-29 <pragathisarathi02@gmail.com>
2026-01-27 12:26:08 +0530

    ETE

--- app/KCE/department-of-electronics-and-telecommunication-engineering/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Page() {
-  return (
-    <div>department-of-electronics-and-telecommunication-engineering</div>
-  )
-}
---
+++ app/KCE/department-of-electronics-and-telecommunication-engineering/page.jsx
@@ -0,0 +1,16 @@
+import DeptOfETEPage from "@/components/00-KCE/Academics/DeptOfETE";
+
+export const metadata = {
+  title: "Best college for Engineering In Tamilnadu - KCE",
+  description: "Unlock your potential in Engineering at KCE, the leading Engineering college in Tamilnadu, providing a nurturing environment for academic and personal growth.",
+};
+
+const page = () => {
+  return (
+    <>
+     <DeptOfETEPage/>
+    </>
+  );
+};
+
+export default page;
