4fccec7fbdc9ae84569670f7b0961eaa486bd944
Pragathi-29 <pragathisarathi02@gmail.com>
2026-01-27 16:49:13 +0530

    Meta Title

--- app/KCE/department-of-information-technology/page.jsx
+++ app/KCE/department-of-information-technology/page.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import DeptOfITPage from "@/components/00-KCE/Academics/DeptOfITPage";
 
-export default function Page() {
+export const metadata = {
+  title: "Best Information Technology Colleges In Coimbatore - KCE",
+  description: "Karpagam College of Engineering is one of the best IT colleges in Coimbator and Elevate your career with KCE & We offer Practical Training with internships.",
+};
+
+const page = () => {
   return (
-    <div>department-of-information-technology</div>
-  )
-}
+    <>
+     <DeptOfITPage/>
+    </>
+  );
+};
+
+export default page;
---
+++ app/KCE/department-of-management-studies/page.jsx
@@ -0,0 +1,16 @@
+import DeptOfMSPage from "@/components/00-KCE/Academics/DeptOfMSPage";
+
+export const metadata = {
+  title: "Management Studies - Best Colleges For MBA In Tamilnadu",
+  description: "KCE is one of the MBA colleges in Tamil Nadu, specializing in Management Studies. Elevate your career with strategic education and best learning opportunities.",
+};
+
+const page = () => {
+  return (
+    <>
+     <DeptOfMSPage/>
+    </>
+  );
+};
+
+export default page;
