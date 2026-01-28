ba82b989bafb969d7d060a77562dab886b0c4476
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-28 13:00:28 +0530

    campus event and health fitness page json updated
    infrastructure/campus-events
    health-and-fitness/

--- app/KCE/infrastructure/campus-events/page.js
+++ app/KCE/infrastructure/campus-events/page.js
@@ -1,7 +1,28 @@
-import React from 'react'
+"use client";
+import React from "react";
+import data from "@/JSON/campus/campus_event.json";
+import InnerBanner from "@/components/00-KCE/common/InnerBanner";
+import RenderSection from "@/components/00-KCE/RenderComponent/RenderSection";
 
-export default function Page() {
+export default function CampusEventPage() {
+  const pageData = data;
+  const renderSectionWrapper = (section, index) => {
+    return (
+      <RenderSection
+        section={section}
+        index={index}
+        renderSection={renderSectionWrapper}
+      />
+    );
+  };
+
   return (
-    <div>campus-events</div>
-  )
+    <>
+      <InnerBanner data={pageData?.banner} />
+
+      {pageData?.sections?.map((section, index) =>
+        renderSectionWrapper(section, index),
+      )}
+    </>
+  );
 }
