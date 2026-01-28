dff1ce3e34467cd0955cedfbd55590bf1edb5b4c
nsathiyakala <nsathiyakala6@gmail.com>
2026-01-28 13:05:35 +0530

    national innovations

--- components/00-KCE/DepartmentCompoennts/RectangleList.jsx
+++ components/00-KCE/DepartmentCompoennts/RectangleList.jsx
@@ -1,18 +1,35 @@
 "use client";
 
+import Link from "next/link";
+
 const RectangleList = ({ data }) => {
   return (
-    <section className="ps-section">
-      <div className="container">
-        <div className="row g-3">
-          {data?.content?.map((item, index) => (
-            <div className="col-lg-6 col-md-6 col-sm-12" key={index}>
-              <a href={item.url} className="ps-card">
-                <span className="ps-title">{item.title}</span>
-                <span className="ps-arrow"></span>
-              </a>
-            </div>
-          ))}
+    <section className={data?.sectionbg || "section-bg1"}>
+      <div className="ps-section section-wid">
+        <div className="">
+          <div className="row g-5">
+            {data?.content?.map((item, index) => (
+              <div className="col-lg-6 col-md-6 col-sm-12" key={index}>
+                {item.url ? (
+                  <Link href={item.url} className="ps-card">
+                    <span className="ps-title">{item.title}</span>
+                    <span className="ps-arrow">
+                      {" "}
+                      <i className="feather-chevrons-right"></i>
+                    </span>
+                  </Link>
+                ) : (
+                  <div className="ps-card">
+                    <span className="ps-title">{item.title}</span>
+                    <span className="ps-arrow">
+                      {" "}
+                      <i className="feather-chevrons-right"></i>
+                    </span>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
--- components/00-KCE/DepartmentCompoennts/TimeLine.jsx
+++ components/00-KCE/DepartmentCompoennts/TimeLine.jsx
@@ -2,34 +2,45 @@
 
 const TimeLine = ({ data }) => {
   return (
-    <section className="mom-section">
-      <div className="container">
-        <div className="mom-header">{data?.title}</div>
+    <section className={data?.sectionbg || "section-bg1"}>
+      <div className="mom-section section-wid">
+        <div className="">
+          {data?.title && <h2 className="section-ti">{data?.title}</h2>}
+          {data?.subTitle && <h2 className="sub-ti">{data?.subTitle}</h2>}
 
-        <div className="row g-4">
-          {data?.content.map((item, index) => (
-            <div className="col-lg-6 col-md-6 col-sm-12" key={index}>
-              <div className="mom-card">
-                <div className="mom-card-inner">
-                  {/* Year Circle */}
-                  <div className="mom-year">{item?.year}</div>
+          <div className="row g-5 pt-3">
+            {data?.content.map((item, index) => (
+              <div className="col-lg-6 col-md-6 col-sm-12" key={index}>
+                <div className="mom-card">
+                  <div className="mom-card-inner">
+                    {/* Year Circle */}
+                    <div className="mom-year">{item?.year}</div>
 
-                  {/* Timeline */}
-                  <div className="mom-timeline">
-                    <span className="mom-vertical-line"></span>
+                    {/* Timeline */}
+                    <div className="mom-timeline">
+                      <span className="mom-vertical-line"></span>
 
-                    {item.dates.map((date, i) => (
-                      <div className="mom-timeline-row" key={i}>
-                        <span className="mom-horizontal-line"></span>
-                        <span className="mom-arrow"></span>
-                        <span className="mom-date">{date}</span>
-                      </div>
-                    ))}
+                      {item.dates.map((date, i) => (
+                        <div className="mom-timeline-row" key={i}>
+                          <span className="mom-horizontal-line"></span>
+                          <span className="mom-arrow"></span>
+
+                          <a
+                            href={date.url}
+                            className="mom-date"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {date.text}
+                          </a>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </section>
