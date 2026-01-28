8499bad67b47c31e1c5ea9332cd42378774e3c62
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-28 15:23:02 +0530

    Transport Cafeteria enterpreneurship Banking KCE/infrastructure

--- app/KCE/infrastructure/entrepreneurship/page.js
+++ app/KCE/infrastructure/entrepreneurship/page.js
@@ -1,7 +1,17 @@
+import EntrepreneurshipPage from '@/components/00-KCE/Campus/entrepreneurship';
+import HealthAndFitnessPage from '@/components/00-KCE/Campus/healthAndFitness';
 import React from 'react'
 
-export default function Page() {
+export const metadata = {
+  title: "Best college for Engineering In Tamilnadu - KCE",
+  description:
+    "Unlock your potential in Engineering at KCE, the leading Engineering college in Tamilnadu, providing a nurturing environment for academic and personal growth.",
+};
+
+const page = () => {
   return (
-    <div>entrepreneurship</div>
+    <EntrepreneurshipPage/>
   )
 }
+
+export default page
