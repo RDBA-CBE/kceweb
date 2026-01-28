f9d7a69f7988058e2897d34e4d7452cef69b9064
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-28 16:02:47 +0530

    MEDIA - photo gallery photo gallery placement - overview placement-procedure

--- components/00-KCE/Placement/placementProcedure.jsx
+++ components/00-KCE/Placement/placementProcedure.jsx
@@ -1,12 +1,12 @@
 
 'use client'
 import React from 'react'
-import data from "@/JSON/PLACEMENT/overview.json";
+import data from "@/JSON/PLACEMENT/placement_procedure.json";
 
 import InnerBanner from '@/components/00-KCE/common/InnerBanner'
 import RenderSection from '@/components/00-KCE/RenderComponent/RenderSection'
 
-export default function OverviewPage () {
+export default function PlacementProcedurePage () {
   const pageData = data
   const renderSectionWrapper = (section, index) => {
     return (
