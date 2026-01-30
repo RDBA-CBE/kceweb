2d99980f39750700fdd47a0395692bf47ccfc7e4
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-30 10:17:16 +0530

    recuirter list page update

--- app/KCE/recruiters-list/page.js
+++ app/KCE/recruiters-list/page.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import OverviewPage from '@/components/00-KCE/Placement/overview'
+import RecuirterListPage from '@/components/00-KCE/Placement/recuirterList'
 
-export default function Page() {
-  return (
-    <div>recruiters-list</div>
-  )
+export const metadata = {
+  title: 'Best college for Engineering In Tamilnadu - KCE',
+  description:
+    'Unlock your potential in Engineering at KCE, the leading Engineering college in Tamilnadu, providing a nurturing environment for academic and personal growth.'
 }
+
+const page = () => {
+  return <RecuirterListPage />
+}
+
+export default page
