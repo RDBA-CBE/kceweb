8b19f7d8c65bc4f0ecb803e9ed2203d80b23dbcd
nsathiyakala <nsathiyakala6@gmail.com>
2026-02-02 17:49:53 +0530

    build issue

--- components/00-KCE/DirectComponents/photoGallery.jsx
+++ components/00-KCE/DirectComponents/photoGallery.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import data from '@/JSON/MEDIA/photoGallery.json'
+import data from '@/JSON/media/photo_gallery.json'
 import InnerBanner from '@/components/00-KCE/common/InnerBanner'
 import RenderSection from '@/components/00-KCE/RenderComponent/RenderSection'
 
