c892244cbaad2f696e7bac007607fc77cc909ab5
Pragathi-29 <148082025+Pragathi-29@users.noreply.github.com>
2026-04-06 15:27:23 +0530

    new pages

---
+++ app/ai-ds-association-activity/page.jsx
@@ -0,0 +1,16 @@
+import AIDSAssociationActivity from "@/components/00-KCE/Academics/AIDSAssociationActivity";
+
+export const metadata = {
+  title: "Top Civil Engineering College in Coimbatore",
+  description: "Join KCE, one of the Top Civil Engineering Colleges in Coimbatore, for applied learning, modern facilities, and excellent placements.",
+};
+
+const page = () => {
+  return (
+    <>
+     <AIDSAssociationActivity/>
+    </>
+  );
+};
+
+export default page;
---
+++ components/00-KCE/Academics/AIDSAssociationActivity.jsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import data from "../../../JSON/ACADEMIC/dept_of_aidsassociationactivity.json";
+import InnerBanner from "@/components/00-KCE/common/InnerBanner";
+import RenderSection from "../RenderComponent/RenderSection";
+
+
+export default function AIDSAssociationActivity() {
+  const civil = data;
+
+  const renderSectionWrapper = (section, index) => {
+    return (
+      <RenderSection
+        key={index}
+        section={section}
+        index={index}
+        renderSection={renderSectionWrapper}
+      />
+    );
+  };
+
+  return (
+    <div>
+      <InnerBanner data={civil?.banner} />
+
+      {civil?.sections?.map((section, index) =>
+        renderSectionWrapper(section, index),
+      )}
+    </div>
+  );
+}
