40b775ae1148f76d950cba8544eaee86f90348d6
IndumathiNavinkumar <rdba.developer@gmail.com>
2026-04-09 16:17:58 +0530

    google tag manager code added

--- app/layout.js
+++ app/layout.js
@@ -20,34 +20,57 @@ import "swiper/css/thumbs";
 // ========= Plugins CSS END =========
 
 import "../public/scss/styles.scss";
+
 import { Provider } from "react-redux";
 import Context from "@/context/Context";
 import Store from "@/redux/store";
+
 import BackToTop from "./backToTop";
 import Separator from "@/components/00-Kahe/common-components/Footer-sub-components/Seperator";
 import MobileMenu from "@/components/00-Kahe/common-components/MobileMenu";
-import HeaderEight from "@/components/Header/Headers/Header-Eight";
-import Footer from "@/components/00-Kahe/common-components/Footer";
 import Header from "@/components/00-Kahe/common-components/Header";
+import Footer from "@/components/00-Kahe/common-components/Footer";
 import BottomPlacement from "@/components/00-KCE/common/BottomPlacement";
-
 
 export default function RootLayout({ children }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
+
   return (
     <html lang="en" dir="ltr">
       <head>
         {/* ✅ GOOGLE FONT – MONA SANS */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
           href="https://fonts.googleapis.com/css2?family=Mona+Sans:ital,wght@0,200..900;1,200..900&display=swap"
           rel="stylesheet"
         />
+
+        {/* ✅ Google Tag Manager Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WJ9V63D');`,
+          }}
+        />
       </head>
-      <body className="" suppressHydrationWarning={true}>
+
+      <body suppressHydrationWarning={true}>
+        {/* ✅ Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WJ9V63D"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Provider store={Store}>
           <Context>
             <MobileMenu />
@@ -55,7 +78,7 @@ export default function RootLayout({ children }) {
             {children}
             <BackToTop />
             {/* <Separator /> */}
-            <BottomPlacement/>
+            <BottomPlacement />
             <Footer />
           </Context>
         </Provider>
