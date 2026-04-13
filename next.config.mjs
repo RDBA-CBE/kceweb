ef15690bb946cafba9977889646bf4db512d44f8
Duraisamy303 <psmkduraisamy@gmail.com>
2026-04-13 16:27:31 +0530

    next config public url redirection updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+
+  async redirects() {
+    return [
+      {
+        source: "/news{/}?",
+        destination: "https://learn.kce.ac.in/category/latest-news/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
