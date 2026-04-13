8ab2af0ff9f7e69bb4e5ddeb2110f493b9ab612f
Duraisamy303 <psmkduraisamy@gmail.com>
2026-04-13 17:02:08 +0530

    remove redirect

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,15 @@
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 
-  async redirects() {
-    return [
-      {
-        source: "/news{/}?",
-        destination: "https://learn.kce.ac.in/category/latest-news/",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/news{/}?",
+  //       destination: "https://learn.kce.ac.in/category/latest-news/",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
