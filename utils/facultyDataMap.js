5e8a122d86c228fcc2dc7ec19aa5b85e103ad3c8
Duraisamy303 <psmkduraisamy@gmail.com>
2026-05-08 10:26:08 +0530

    code updtaed

--- utils/facultyDataMap.js
+++ utils/facultyDataMap.js
@@ -1,19 +1,21 @@
-import aids from "@/JSON/ACADEMIC/dept_of_aids.json";
+// import aids from "@/JSON/ACADEMIC/dept_of_aids.json";
 import civil from "@/JSON/ACADEMIC/dept_of_civil.json";
-import csd from "@/JSON/ACADEMIC/dept_of_csd.json";
-import cse from "@/JSON/ACADEMIC/dept_of_cse.json";
-import cseCyber from "@/JSON/ACADEMIC/dept_of_cse_cyber.json";
-import cst from "@/JSON/ACADEMIC/dept_of_cst.json";
-import ece from "@/JSON/ACADEMIC/dept_of_ece.json";
-import eee from "@/JSON/ACADEMIC/dept_of_eee.json";
-import ete from "@/JSON/ACADEMIC/dept_of_ete.json";
-import it from "@/JSON/ACADEMIC/dept_of_it.json";
-import mca from "@/JSON/ACADEMIC/dept_of_mca.json";
-import mech from "@/JSON/ACADEMIC/dept_of_mechengg.json";
-import ms from "@/JSON/ACADEMIC/dept_of_ms.json";
-import sciHum from "@/JSON/ACADEMIC/dept_of_sci_hum.json";
+// import csd from "@/JSON/ACADEMIC/dept_of_csd.json";
+// import cse from "@/JSON/ACADEMIC/dept_of_cse.json";
+// import cseCyber from "@/JSON/ACADEMIC/dept_of_cse_cyber.json";
+// import cst from "@/JSON/ACADEMIC/dept_of_cst.json";
+// import ece from "@/JSON/ACADEMIC/dept_of_ece.json";
+// import eee from "@/JSON/ACADEMIC/dept_of_eee.json";
+// import ete from "@/JSON/ACADEMIC/dept_of_ete.json";
+// import it from "@/JSON/ACADEMIC/dept_of_it.json";
+// import mca from "@/JSON/ACADEMIC/dept_of_mca.json";
+// import mech from "@/JSON/ACADEMIC/dept_of_mechengg.json";
+// import ms from "@/JSON/ACADEMIC/dept_of_ms.json";
+// import sciHum from "@/JSON/ACADEMIC/dept_of_sci_hum.json";
 
-const allDepts = [aids, civil, csd, cse, cseCyber, cst, ece, eee, ete, it, mca, mech, ms, sciHum];
+// const allDepts = [aids, civil, csd, cse, cseCyber, cst, ece, eee, ete, it, mca, mech, ms, sciHum];```
+const allDepts = [ civil];
+
 
 function extractFacultyData(deptJson) {
   const sections = deptJson?.sections || [];
