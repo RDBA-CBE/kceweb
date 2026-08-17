af7bdba97f6f230b911f005a332569b2cf0a52a3
IndumathiNavinkumar <rdba.developer@gmail.com>
2026-08-17 17:42:49 +0530

    Home banner added

--- components/00-KCE/Home/HeroSection/HeroSection.jsx
+++ components/00-KCE/Home/HeroSection/HeroSection.jsx
@@ -11,7 +11,7 @@ export default function HeroSection() {
       {/* Background image slider */}
       <BackgroundSlider
         images={[
-          
+           "/images/kce/home/banner/kce-b1.png",
           "/images/kce/home/banner/b1.jpg",
           "/images/kce/home/banner/banner-img2.webp",
           "/images/kce/home/banner/banner-img3.webp",
